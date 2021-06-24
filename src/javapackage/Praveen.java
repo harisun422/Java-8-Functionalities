226cd4f961cfc8c0f5dbfce2bdf8ed5f22e95103
delluser <delluser@LAPTOP-6N2GONVC>
2021-06-24 22:51:15 +0530

    firsttime

---
+++ src/javapackage/Praveen.java
@@ -0,0 +1,10 @@
+package javapackage;
+
+public class Praveen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n\n\n Hello World! \n\n\n");
+	}
+
+}
